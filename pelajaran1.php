3aabf098ef152c482e76814a52c26d7e556633b0
santia <santiatia2003@gmail.com>
2019-08-01 17:03:33 +0700

    isi

---
+++ pelajaran1.php
@@ -0,0 +1,8 @@
+<?php
+$harga = 15000;
+$dus = 6;
+$berisi = 5;
+$jualdus = (6*2000);
+$perhitungan = ($harga*$dus*$berisi)+$jualdus;
+echo "jumlah seluruhnya = $perhitungan";
+?>
---
+++ umur.php
@@ -0,0 +1,16 @@
+<?php
+$tama = 1;
+$toni = 0.5;
+$tinitama = 14;
+$tinitoni = 34;
+
+echo "Umur tama $tama X<br>";
+echo "Umur toni $Toni X<br>";
+echo "Umur Toni + Umur tini = $tinitoni<br>";
+echo "$toni + $tamaX - $tinitama = $tinitoni<br>";
+$x = $tama + $toni;
+$y = $tinitama + $tinitoni;
+$z = $y/$x;
+echo " $x X = $tinitama + $tinitoni<br>";
+
+?>
