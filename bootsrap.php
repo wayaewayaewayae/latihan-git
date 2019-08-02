e88ade3c2983edf613b1974964e42b7a802aa81d
Santia <Santiatia2003@gmail.com>
2019-08-02 15:07:25 +0700

    html dalam php

---
+++ bootsrap.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Belajar boo</title>
+</head>
+<body>
+<?php
+$a = 5;
+$b = 1;
+$c = $a+$b;
+
+if($c >= 6){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 4){ 
+echo '<h1 style ="color : glue">' .$c .'</h1>';
+}
+
+$a = 3;
+$b = 4;
+$c = $a+$b;
+
+if($c >= 7){
+echo '<h1 style ="color : yellow">' .$c .'</h1>';
+}elseif ($c <= 5){ 
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}
+
+$a = 5;
+$b = 7;
+$c = $a+$b;
+
+if($c >= 12){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 11){ 
+echo '<h1 style ="color : yellow">' .$c .'</h1>';
+}
+
+$a = 25;
+$b = 7;
+$c = $a+$b;
+
+if($c >= 27){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 22){ 
+echo '<h1 style ="color : blue">' .$c .'</h1>';
+}
+
+$a = 34;
+$b = 1;
+$c = $a+$b;
+
+if($c >= 35){
+echo '<h1 style ="color : black">' .$c .'</h1>';
+}elseif ($c <= 25){ 
+echo '<h1 style ="color : brown">' .$c .'</h1>';
+}
+
+$a = 2;
+$b = 24;
+$c = $a+$b;
+
+if($c >= 26){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 21){ 
+echo '<h1 style ="color : yellow">' .$c .'</h1>';
+}
+
+$a = 5;
+$b = 26;
+$c = $a+$b;
+
+if($c >= 30){
+echo '<h1 style ="color : yellow">' .$c .'</h1>';
+}elseif ($c <= 27){ 
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}
+
+$a = 2;
+$b = 24;
+$c = $a+$b;
+
+if($c >= 26){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 25){ 
+echo '<h1 style ="color : black">' .$c .'</h1>';
+}
+
+$a = 17;
+$b = 0;
+$c = $a+$b;
+
+if($c >= 17){
+echo '<h1 style ="color : glue">' .$c .'</h1>';
+}elseif ($c <= 15){ 
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}
+
+$a = 2;
+$b = 4;
+$c = $a+$b;
+
+if($c >= 6){
+echo '<h1 style ="color : red">' .$c .'</h1>';
+}elseif ($c <= 5){ 
+echo '<h1 style ="color : yellow">' .$c .'</h1>';
+}
+
+
+?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+</body>
+</html>
