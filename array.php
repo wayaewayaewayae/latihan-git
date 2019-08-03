43686754ad964d02ae7210aba9b8cd2a9a8e2696
Santia <Santiatia2003@gmail.com>
2019-08-03 16:08:52 +0700

    array

---
+++ array.php
@@ -0,0 +1,6 @@
+<?php
+$b = 'juli';
+$a = [1,2,4, 'hello',100,200,500,800,0,'Santia',$b];
+for ($i=0; $i < count($a); $i++) { 
+    echo 'index array ke-' .$i . 'Dengan isi data ' . $a[$i] . '<br>';}
+    ?>
---
+++ ulangan.php
@@ -0,0 +1,23 @@
+<?php
+$a =1;
+echo "$a x $a = $a<br>";
+$b =2;
+echo "$a x $b = $b<br>";
+$c =3;
+echo " x 3 = $c<br>";
+$d =4;
+$e =5;
+$f =6;
+$g =7;
+$h =8;
+$i =9;
+$j =10;
+echo "1 x 3 = $c<br>";
+echo "1 x 4 = $d<br>";
+echo "1 x 5 = $e<br>";
+echo "1 x 6 = $f<br>";
+echo "1 x 7 = $g<br>";
+echo "1 x 8 = $h<br>";
+echo "1 x 9 = $i<br>";
+echo "1 x 10 = $j<br>";
+?>
